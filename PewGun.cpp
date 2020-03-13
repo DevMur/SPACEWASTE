010d5dc6b7f0782247eb57966d6f27a5d2d85ac2
DevMur <60196179+DevMur@users.noreply.github.com>
2020-03-13 18:55:20 -0400

    Functionality for movement pew gun and basic enemy
    TODO:
    enemy 2 and 3
    gun 2 and 3
    score board / menu / gameplay
    boss 1 and 2
    sprites and sound

---
+++ PewGun.cpp
@@ -0,0 +1,43 @@
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include "PewGun.h"
+#include "Enemy.h"
+
+PewGun::PewGun(){
+    //draw the bullet, to be edited/cleaned
+    setRect(20, 0, 10, 50);
+
+    //connect to ship
+    QTimer * gun_timer = new QTimer();
+    PewGun::connect(gun_timer, SIGNAL(timeout()), this, SLOT(move()));
+
+    gun_timer->start(50); //rates to be edited based on play testing and how it looks
+}
+
+void PewGun::move(){
+    //destroy enemies
+    QList<QGraphicsItem *> colliding_items = collidingItems(); //houses all items the bullet item is colliding with
+    for (int i = 0, collisions_min_found = colliding_items.size(); i < collisions_min_found; i++)
+    {
+        if (typeid (*colliding_items[i]) == typeid(Enemy))
+        {
+            scene()->removeItem(colliding_items[i]); //must also add points here
+            scene()->removeItem(this);
+            //delete from memory? stackoverflow says this is standard;
+            delete colliding_items[i];
+            delete this;
+            return;
+        }
+    }
+
+
+    //let the bullet fly, rate tbd on testing and games vibe
+    setPos(x(), y()-10);
+    if (pos().y() + rect().height() < 0)
+    {
+        scene()->removeItem(this);
+        delete this;
+        //qDebug() << "bullet off screen";
+    }
+}
---
+++ enemy.cpp
@@ -0,0 +1,37 @@
+#include "Enemy.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include <stdlib.h>
+
+Enemy::Enemy(){
+    //TODO: make sprites
+    //      adjust sizing and speed from testing
+    //      adjust spawn rate
+    //      add points
+    //      destroy on impact
+
+    //set random position
+    int random_int = rand() % 700;
+    setPos(random_int, 0);
+
+    //draw the enemy
+    setRect(0, 0, 100, 100);
+
+    //connect to ship
+    QTimer * enemy_gun_timer = new QTimer();
+    Enemy::connect(enemy_gun_timer, SIGNAL(timeout()), this, SLOT(move()));
+
+    enemy_gun_timer->start(50); //rates to be edited based on play testing and how it looks
+}
+
+void Enemy::move(){
+    //move easy enemy down
+    setPos(x(), y()+5);
+    if (pos().y() + rect().height() < 0)
+    {
+        scene()->removeItem(this);
+        delete this;
+        //qDebug() << "bullet off screen";
+    }
+}
