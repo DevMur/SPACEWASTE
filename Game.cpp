0f8c6e9a048381ae26134a26802c0e1e79e6e16d
DevMur <60196179+DevMur@users.noreply.github.com>
2020-04-10 17:49:29 -0600

    Game update. Runs with a parent widget

--- Game.cpp
+++ Game.cpp
@@ -1,44 +1,49 @@
-#include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
+#include "Game.h"
 #include <QTimer>
-//#include <QGraphicsItem>
-#include "MyRect.h"
-#include "PewGun.h"
+#include <QGraphicsTextItem>
+#include <QFont>
+#include "Enemy.h"
+#include <QMediaPlayer>
+#include <QBrush>
+#include <QImage>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    // create scene
-    QGraphicsScene * scene = new QGraphicsScene();
-    //create an item for the scene.
+Game::Game(QWidget *parent){
 
-    MyRect * player = new MyRect(); //length and width of 0 (default)
-    player->setRect(0,0,50,50); // top left , bottom right
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600); // make the scene 800x600
+    setBackgroundBrush(QBrush(QImage(":/images/bg.png"))); //background, this will be changed with levels
 
-    scene->addItem(player); //item added to scene.
+
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+    //should this all be called from the level creator or just the first part.
+    //this is where levels are called. Level completion key (potentially save states lol)
+
+
+    player = new Player();
+    player->setPos(350,500); // Always be in the middle bottom of screen
     player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
-    player->grabKeyboard();
+    scene->addItem(player);
+    //create a ship for the player
 
+    score = new Score();
+    scene->addItem(score);
+    health = new Health();
+    health->setPos(health->x(),health->y()+25); //not where this will go, good for testing
+    scene->addItem(health);
+    //add health TODO set 3 = 3 ships on the scene, 2 = 2 ships, etc.
+    //must be called for each level
 
-    //add view
-    QGraphicsView * view = new QGraphicsView(scene);
-    //select the size of the window and the scene.
-    view->setFixedSize(800, 800);
-    scene->setSceneRect(0,0, 800, 800);
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//player was moved to the center to better understand how the view vs scene works
-    //this method is used as a more conventional way to restart lives/the game.
+    QTimer * timer = new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
+    timer->start(2000);
 
-    player->setPos(view->width()/2 - player->rect().width()/2, view->height() - player->rect().height());
-
-
-    QTimer * enemy_timer = new QTimer;
-    QObject::connect(enemy_timer, SIGNAL(timeout()), player, SLOT(spawn()));
-    enemy_timer->start(2000);
-
-    view->show();
-    return a.exec();
+//    QMediaPlayer * music = new QMediaPlayer();
+//    music->setMedia(QUrl(""));
+//    music->play();
+//New music for each level? Just bosses? End music? TODO ask for more songs from Jordan
+    show();
 }
