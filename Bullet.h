9f6c02d7282af4556f0bc049d69021d0d898d549
DevMur <60196179+DevMur@users.noreply.github.com>
2020-04-10 17:53:35 -0600

    It's got an image now.
    Should weapons stack

---
+++ Bullet.h
@@ -0,0 +1,22 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsItem>
+#include <QObject>
+
+class Bullet: public QObject, public QGraphicsPixmapItem {
+    Q_OBJECT
+public:
+    Bullet(QGraphicsItem * parent=0);
+public slots:
+    void move();
+};
+//Pretty straight forward. A bullet with a shape gets created.
+//it's only job is to move
+//while moving it may come in contact with something or reach EOF
+//it needs rules to it's movement and rules for destruction
+
+//INQUIRED ABOUT: stack bullet items into 4 parallel
+
+#endif // BULLET_H
